ad67970cf9ba1b077c3e6edf4b33ce6643a22f4c
alireza <malirezag@gmail.com>
2025-04-06 13:56:44 +0330

    d

--- src/ui/ProtectedRoute.jsx
+++ src/ui/ProtectedRoute.jsx
@@ -1,13 +1,12 @@
-import { Outlet, useNavigate } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 import useGetUser from "../features/doctors/authentication/useGetUser";
 import { useEffect } from "react";
 
 function ProtectedRoute() {
-  const navigate = useNavigate();
   const { user } = useGetUser();
   useEffect(() => {}, [user?.role]);
 
-  return user?.role === "authenticated" ? <Outlet /> : navigate("/");
+  return user?.role === "authenticated" ? <Outlet /> : Navigate("/");
 }
 
 export default ProtectedRoute;
